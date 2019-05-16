501dd89ca28f37478e06a4e93ac03d51a7b652f0
henrique.souza <henrique.souza@trtrio.gov.br>
2019-05-16 10:50:14 -0300

    Removed Admin Server API

--- hfsframework-admin-api/src/main/java/br/com/hfsframework/admin/repository/IParametroCategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.hfsframework.admin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.hfsframework.admin.domain.ParametroCategoria;
-
-public interface IParametroCategoriaRepository extends JpaRepository<ParametroCategoria, Long> {
-
-}
--- hfsframework-admin-api/src/main/java/br/com/hfsframework/admin/repository/IParametroRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.hfsframework.admin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.hfsframework.admin.domain.Parametro;
-
-public interface IParametroRepository extends JpaRepository<Parametro, Long> {
-
-}
