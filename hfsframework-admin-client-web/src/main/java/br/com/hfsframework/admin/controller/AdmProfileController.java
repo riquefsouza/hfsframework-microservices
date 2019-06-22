6ec74652c795d62485001146a799abc4318b5cb1
Administrador <Administrador@TRT183754.trtrio.gov.br>
2019-06-22 13:49:27 -0300

    Added picklist to Adm Profile page.

--- hfsframework-admin-client-web/src/main/java/br/com/hfsframework/admin/controller/AdmProfileController.java
+++ hfsframework-admin-client-web/src/main/java/br/com/hfsframework/admin/controller/AdmProfileController.java
@@ -2,12 +2,21 @@ package br.com.hfsframework.admin.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.Callable;
 
 import javax.annotation.PostConstruct;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.hfsframework.admin.client.AdmPageRestClient;
 import br.com.hfsframework.admin.client.AdmProfileRestClient;
@@ -47,6 +56,11 @@ public class AdmProfileController extends BaseViewRegisterRestClient<AdmProfileD
 		this.listAllAdmUsers = new ArrayList<AdmUserDTO>();
 	}
 
+	@PostConstruct
+	public void init() {
+		//
+	}
+	
 	private void carregarAdmPages(AdmProfileDTO bean, boolean bEdit) {
 		List<AdmPageDTO> listAdmPagesSelected;		 
 		List<AdmPageDTO> listAdmPages = admPageClient.getAll();
@@ -77,8 +91,86 @@ public class AdmProfileController extends BaseViewRegisterRestClient<AdmProfileD
 		this.dualListAdmUser = new BaseDualList<AdmUserDTO>(listAdmUsers, listAdmUsersSelected);		
 	}	
 	
-	@PostConstruct
-	public void init() {
-		//
+	@Override
+	@GetMapping("/add")	
+	public ModelAndView add(AdmProfileDTO bean) {
+		Optional<ModelAndView> mv = getPage(getEditPage());
+		bean.clear();
+				
+		if (mv.isPresent()) {
+			admPageClient.init(this.resourceServer, this.accesToken);
+			carregarAdmPages(bean, false);
+			mv.get().addObject("listSourceAdmPages", this.dualListAdmPage.getSource());
+
+			admUserClient.init(this.resourceServer, this.accesToken);
+			carregarAdmUsers(bean, false);
+			mv.get().addObject("listSourceAdmUsers", this.dualListAdmUser.getSource());
+			
+			mv.get().addObject("bean", bean);
+		}
+		
+		return mv.get();
 	}
+	
+	@Override
+	@GetMapping("/edit")
+	public ModelAndView edit(AdmProfileDTO bean) {
+		Optional<ModelAndView> mv = getPage(getEditPage());
+
+		Optional<AdmProfileDTO> obj = bean.fromJSON();
+		if (obj.isPresent()) {
+			bean = obj.get();
+			
+			if (mv.isPresent()) {
+				admPageClient.init(this.resourceServer, this.accesToken);
+				carregarAdmPages(bean, true);
+				mv.get().addObject("listSourceAdmPages", this.dualListAdmPage.getSource());
+				mv.get().addObject("listTargetAdmPages", this.dualListAdmPage.getTarget());
+
+				admUserClient.init(this.resourceServer, this.accesToken);
+				carregarAdmUsers(bean, true);
+				mv.get().addObject("listSourceAdmUsers", this.dualListAdmUser.getSource());
+				mv.get().addObject("listTargetAdmUsers", this.dualListAdmUser.getTarget());
+				
+				mv.get().addObject("bean", bean);
+			}
+		}
+		
+		return mv.get();
+	}
+	
+	@Override
+	@PostMapping
+	public ModelAndView save(@Valid @ModelAttribute AdmProfileDTO bean, 
+			BindingResult result, RedirectAttributes attributes) {
+	
+		Optional<ModelAndView> mv = getPage(this.getListPage());
+		
+		return this.saveCallableBefore(bean, result, attributes, new Callable<String>() {
+			
+			@Override
+			public String call() throws Exception {
+				
+				if (mv.isPresent()) {
+					
+					bean.getAdmPages().forEach(item -> {
+						listAllAdmPages.stream()
+							.filter(p -> p.getId().equals(item.getId()))
+							.findFirst()
+							.ifPresent(x -> item.setDescription(x.getDescription()));	
+					});
+					bean.getAdmUsers().forEach(item -> {
+						listAllAdmUsers.stream()
+							.filter(p -> p.getId().equals(item.getId()))
+							.findFirst()
+							.ifPresent(x -> item.setName(x.getName()));	
+					});
+					
+					mv.get().addObject("bean", bean);
+				}
+				
+				return "";
+			}
+		});
+	}	
 }
--- hfsframework-admin-client-web/src/main/webapp/WEB-INF/static/js/admin/admProfile/editAdmProfile.js
+++ hfsframework-admin-client-web/src/main/webapp/WEB-INF/static/js/admin/admProfile/editAdmProfile.js
@@ -6,9 +6,49 @@ class EditAdmProfile extends HFSSystemUtil {
 		this.hideQueryString();
 		
 		this._saveMethod = this.getPersistedItem("saveMethod");
-				
+
+		this._admProfilePickListUsers = $('#admProfile_pickListUsers');
+		this._editAdmProfileMessageSourceCaptionUsers = $('#editAdmProfile-message-sourceCaptionUsers').text();
+		this._editAdmProfileMessageTargetCaptionUsers = $('#editAdmProfile-message-targetCaptionUsers').text();
+
+		this._admProfilePickListPages = $('#admProfile_pickListPages');
+		this._editAdmProfileMessageSourceCaptionPages = $('#editAdmProfile-message-sourceCaptionPages').text();
+		this._editAdmProfileMessageTargetCaptionPages = $('#editAdmProfile-message-targetCaptionPages').text();
+
+		this.buildPickListUsers(this._editAdmProfileMessageSourceCaptionUsers, 
+				this._editAdmProfileMessageTargetCaptionUsers);
+
+		this.buildPickListPages(this._editAdmProfileMessageSourceCaptionPages, 
+				this._editAdmProfileMessageTargetCaptionPages);
+		
 	}
 	
+	buildPickListUsers(messageSourceCaptionUsers, messageTargetCaptionUsers){
+		this._admProfilePickListUsers.puipicklist({
+	        showSourceControls: false,
+	        showTargetControls: false,
+	        sourceCaption: messageSourceCaptionUsers,
+	        targetCaption: messageTargetCaptionUsers,
+	        filter: true,
+	        filterMatchMode: "contains",
+	        responsive: true
+	        //sourceData: data
+	    });		
+	}
+
+	buildPickListPages(messageSourceCaptionPages, messageTargetCaptionPages){
+		this._admProfilePickListPages.puipicklist({
+	        showSourceControls: false,
+	        showTargetControls: false,
+	        sourceCaption: messageSourceCaptionPages,
+	        targetCaption: messageTargetCaptionPages,
+	        filter: true,
+	        filterMatchMode: "contains",
+	        responsive: true
+	        //sourceData: data
+	    });		
+	}
+
 	btnCancelClick(event) {
 		event.preventDefault();
 		
